7213f0411ae541b5edf4b4cac498de715cb12ad0
Ricardo Montaño <montanor@javeriana.edu.co>
2014-05-14 20:46:11 -0500

    Update game_control_table_images.php

--- game_control_table_images.php
+++ game_control_table_images.php
@@ -7,20 +7,20 @@ if(isset($_SESSION["user_name"]))
 	$query = mysqli_query($con, $select);
 	$rows = mysqli_num_rows($query);
 	$cont = 0;
+	$tableval;
 	if($rows > 0)
 	{
 		while($row = mysqli_fetch_array($query))
 		{
-			if($cont % 6 == 0) echo "<tr>" 
 			if($row['personaje_activo'] = "T")
 			{
-				echo "<td><img src=\"".$row['id_personaje']."\"></img></td>"
+				=$tableval."<td><img src=\"".$row['id_personaje']."\"></img></td>";
 			}
 			else
 			{
-				echo "<td><img src=\"categoria/down.jpg\"></img></td>"
+				=$tableval."<td><img src=\"categoria/down.jpg\"></img></td>";
 			}
-			if($cont % 6 == 0) echo "</tr>" 
+			if($cont % 6 == 0 && $cont != 0) echo "<tr>".$tableval."</tr>"; 
 			$cont++;
 		}
 	}
